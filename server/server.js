1344d6bf1ae8ecb3ff86888aab54dddede51ce66
David Hong <davydhong@gmail.com>
2018-09-19 14:41:32 -0700

    added cors middleware to the server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-const express = require ('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const {getArtist} = require('../database/index.js')
-
+const { getArtist } = require('../database/index.js');
+const cors = require('cors');
 const server = express();
 
 server.use(bodyParser.json());
-server.use(express.urlencoded({extended: true}));
+server.use(cors());
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../public')));
 
 server.get('/artists/albums/:artistID', (req, res) => {
-  getArtist(req.params.artistID, (data) => {
+  getArtist(req.params.artistID, data => {
     res.send(data);
-  })
+  });
 });
 
 module.exports = server;
