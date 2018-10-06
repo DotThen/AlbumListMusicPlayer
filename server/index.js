e42b2583fa37f7c3041de173ad5af9204a9807c0
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-10-06 14:16:03 -0700

    set up clustering in server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 require('newrelic');
+const cluster = require('cluster');
+const os = require('os');
 const server = require('./server.js');
 
-server.listen(3001, () => console.log("Listening on port 3001!"));
+if (cluster.isMaster) {
+  const cpuCount = os.cpus().length;
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork();
+  }
+} else { 
+  server.listen(3001, () => console.log("Listening on port 3001!"));
+}
+
+cluster.on('exit', (worker) => {
+  console.log('Hey my guy worker ' + worker.id + ' just clocked out');
+  cluster.fork;
+});
