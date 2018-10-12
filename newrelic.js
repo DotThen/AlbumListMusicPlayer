7a21ecbf026b1661f7d8f3a7a4a4112365332714
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-10-11 21:10:55 -0700

    changed env source

--- newrelic.js
+++ newrelic.js
@@ -5,7 +5,6 @@
  * See lib/config/default.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-require('dotenv').config();
 exports.config = {
   /**
    * Array of application names.
